c3112de7b6898d1141d8b3af481a5c00bada8a3b
shejanNizam <nizam.shejan11@gmail.com>
2026-01-02 11:22:17 +0600

    initialize with vite & tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold text-purple-500">
+        Hello from tasker
+      </div>
+    </>
+  );
+}
+
+export default App;
